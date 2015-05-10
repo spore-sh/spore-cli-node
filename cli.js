dc14d37aa4fdf5911b8eca9af7a888e87f428a3b
Trey Griffith <trey.griffith@gmail.com>
2015-05-10 13:14:33 -0700

    add `new` and `set` commands to cli

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var program = require('commander'),
+    path = require('path'),
     Envy = require('./envy'),
     envy = new Envy(),
     pkg = require('./package.json'),
@@ -33,12 +34,12 @@ program
     var email = options.email || readline.question("Email: "),
         password = options.password || readline.question("Password: ", { hideEchoBack: true });
 
-    if(!email || !password) error(new Error("Email and password are required."));
+    if(!email || !password) return error(new Error("Email and password are required."));
 
     envy.signup(email, password, function (err, user) {
-      if(err) error(err);
+      if(err) return error(err);
 
-      log("Account created for " + user.email + ".");
+      info("Account created for " + user.email + ".");
     });
   });
 
@@ -50,7 +51,7 @@ program
   .action(function (options) {
 
     login(options.email, options.password, function (_, user) {
-      log("Logged in as " + user.email + ".");
+      info("Logged in as " + user.email + ".");
     });
   });
 
@@ -59,19 +60,115 @@ program
   .description("List all the apps you have access to")
   .action(function () {
     ensureLogin(function () {
-      envy.listApps(function (err, apps) {
-        if(err) error(err);
+      envy.api.listApps(function (err, apps) {
+        if(err) return error(err);
 
         if(apps.length) {
-          log("Apps:\n" + apps.map(function (app) {
+          info("Apps:\n" + apps.map(function (app) {
             return app.name;
           }).join("\n"));
         } else {
-          warn("No Apps found. Create one with `envy new`.");
+          warn("No Apps found.");
+          help("Create a new app with `envy new`.");
         }
       });
     });
   });
+
+program
+  .command('new [dir]')
+  .alias('init [dir]')
+  .option('-a, --app <appName>', "Name of the application")
+  .description("Add a new app to Envy")
+  .action(function (dir, options) {
+    ensureLogin(function () {
+      var appName = options.appName;
+      dir = path.resolve(process.cwd(), dir);
+
+      if(!appName) {
+        appName = envy.lookupName(dir);
+      }
+
+      envy.api.createApp(appName, function (err, app) {
+        if(err.message === "App already exists.") {
+
+        }
+
+        envy.writeDotEnvy(app.name, dir, function (err) {
+          if(err) return error(err);
+
+          info(app.name + " created.");
+        });
+      });
+    });
+  });
+
+program
+  .command('set [KEY=value]')
+  .option('-a, --app <appName>', "Name of the application")
+  .option('-e, --env <envName>', "Name of the environment")
+  .description("Set an environment variable")
+  .action(function (set, options) {
+    if(set.indexOf('=') === -1) {
+      return error(new Error("A set operation should be of the form ENV_VARIABLE=value"));
+    }
+    ensureLogin(function () {
+      var key,
+          value;
+
+      key = set.substring(0, set.indexOf('='));
+      value = set.substring(set.indexOf('=') + 1);
+
+      getAppAndEnv(process.cwd(), options.appName, options.envName, function (appName, envName) {
+        envy.api.set(appName, envName, key, value, function (err) {
+          if(err) return error(err);
+
+          log("`" + key + "` updated to `" + value + "` for app: `" + appName + "` in environment: `" + envName + "`.");
+
+          // only write the dot-envy back to this directory
+          // if it's the right app and environment
+          // (i.e. they weren't setting a variable for a different app)
+          getAppAndEnv(process.cwd(), null, null, function (defaultApp, defaultEnv) {
+            if(defaultApp === appName && defaultEnv === envName) {
+              envy.writeDotEnvy(appName, envName, process.cwd(), function (err) {
+                log("local environment variables updated.");
+              });
+            } else {
+              help("`" + key + "` was not updated locally because the local app/environment of " + defaultApp + "/" + defaultEnv + " didn't match the one you set.");
+            }
+          });
+        });
+      });
+    });
+  });
+
+function getAppAndEnv(dir, appName, envName, callback) {
+  if(appName && envName) {
+    return callback(appName, envName);
+  }
+
+  envy.readDotEnvy(dir, function (err, vars) {
+    if(err) return error(err);
+
+    if(!appName) {
+      appName = vars.ENVY_APP_NAME;
+    }
+
+    if(!appName) {
+      appName = envy.lookupName(dir);
+    }
+
+    if(!envName) {
+      envName = vars.ENVY_ENV_NAME;
+    }
+
+    if(!envName) {
+      envName = envy.defaultEnv;
+    }
+
+    callback(appName, envName);
+  });
+}
 
 function ensureLogin(callback) {
   if(envy.getKey()) {
@@ -85,7 +182,7 @@ function login(email, password, callback) {
   email = email || readline.question("Email: ");
   password = password || readline.question("Password: ", { hideEchoBack: true });
 
-  if(!email || !password) error(new Error("Email and password are required."));
+  if(!email || !password) return error(new Error("Email and password are required."));
 
   envy.login(email, password, function (err, user) {
     if(err) error(err);
@@ -96,11 +193,14 @@ function login(email, password, callback) {
 
 function error(err) {
   console.error(err.message.error);
-  process.exit(1);
 }
 
-function log(message) {
+function info(message) {
   console.info(message.info);
+}
+
+function help(message) {
+  console.log(message.help);
 }
 
 function warn(message) {
--- envy.js
+++ envy.js
@@ -1,10 +1,14 @@
 var EnvyApi = require('envy-api'),
+    path = require('path'),
+    fs = require('fs'),
     netrc = require('netrc-rw');
 
 module.exports = Envy;
 
 function Envy(options) {
   this.api = new EnvyApi(options);
+  this.dotEnvy = ".envy";
+  this.defaultEnv = "development";
 }
 
 Envy.prototype.signup = function (email, password, callback) {
@@ -37,8 +41,75 @@ Envy.prototype.login = function (email, password, callback) {
   });
 };
 
-Envy.prototype.listApps = function (callback) {
-  this.api.listApps(callback);
+Envy.prototype.readDotEnvy = function (dir, callback) {
+  fs.readFile(path.join(dir, this.dotEnvy), { encoding: 'utf8' }, function (err, contents) {
+    var vars = {};
+
+    if(err && err.code === 'ENOENT') {
+      // dot envy does not exist, so send back
+      // an empty assignment
+      return callback(null, vars);
+    }
+
+    if(err) return callback(err);
+
+    try {
+      contents.split("\n").forEach(function (line, i) {
+
+        // remove comments
+        if(line.indexOf('#') > -1) {
+          line = line.substring(0, line.indexOf('#'));
+        }
+
+        var key,
+            value;
+
+        if(line.indexOf('=') === -1) {
+          throw new Error("Invalid assignment on line " + i + ". " + line + " does not contain an `=`.");
+        }
+
+        key = line.substring(0, line.indexOf('='));
+        value = line.substring(line.indexOf('=') + 1);
+
+        vars[key] = value;
+      });
+    } catch(e) {
+      return callback(e);
+    }
+
+    callback(null, vars);
+  });
+};
+
+Envy.prototype.writeDotEnvy = function (appName, envName, dir, callback) {
+  if(arguments.length < 4) {
+    callback = dir;
+    dir = envName;
+    envName = this.defaultEnv;
+  }
+
+  var dotEnvy = this.dotEnvy;
+
+  this.api.getDotEnvy(appName, envName, function (err, contents) {
+    if(err) return callback(err);
+
+    fs.writeFile(path.join(dir, dotEnvy), contents, callback);
+  });
+};
+
+Envy.prototype.lookupName = function (dir) {
+  // check for node.js package name
+  try {
+    return require(path.join(dir, "package.json")).name;
+  } catch(e) {
+    // not a valid node application
+  }
+  
+  // check for Rails application
+  // TODO
+  
+  // fallback on directory name
+  return path.basename(dir);
 };
 
 Envy.prototype._setKey = function (email, key) {
